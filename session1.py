8694449a157b35435b2e7e46800afa59b7a03a26
aazambrano2 <aazambrano2@miners.utep.edu>
2025-02-19 12:27:59 -0700

    pb12 v2

--- session1.py
+++ session1.py
@@ -725,3 +725,48 @@ running_sum(superhero_stats)
 
 superhero_stats = [3, 1, 2, 10, 1]
 running_sum(superhero_stats)
+
+"""
+
+Problem 12: Shuffle
+Write a function shuffle() that accepts a list cards of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn]. 
+Return the list in the form [x1,y1,x2,y2,...,xn,yn].
+
+def shuffle(cards):
+	pass
+Example Usage
+
+cards = ['Joker', 'Queen', 2, 3, 'Ace', 7]
+shuffle(cards)
+
+cards = [9, 2, 3, 'Joker', 'Joker', 3, 2, 9]
+shuffle(cards)
+
+cards = [10, 10, 2, 2]
+shuffle(cards)
+Example Output:
+
+['Joker', 3, 'Queen', 'Ace', 2, 7]
+[9, 'Joker', 2, 3, 3, 2, 'Joker', 9]
+[10, 2, 10, 2]
+
+"""
+
+def shuffle(cards):
+
+    n = len(cards) // 2
+    ans = []
+
+    for i in range(n):
+        ans.append(cards[i])
+        ans.append(cards[i + n])
+    print(ans)
+    return ans
+cards = ['Joker', 'Queen', 2, 3, 'Ace', 7]
+shuffle(cards)
+
+cards = [9, 2, 3, 'Joker', 'Joker', 3, 2, 9]
+shuffle(cards)
+
+cards = [10, 10, 2, 2]
+shuffle(cards)
