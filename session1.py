82724c807098cbcf77a091bea9222e045910d0f5
aazambrano2 <aazambrano2@miners.utep.edu>
2025-02-19 11:38:38 -0700

    p2 v2

--- session1.py
+++ session1.py
@@ -383,3 +383,33 @@ def batman():
     print("I am vengeance. I am the night. I am Batman!")
 
 batman()
+
+"""
+Problem 2: Mad Libs
+Write a function madlib() that accepts one parameter, a string verb. 
+The function should print the sentence: "I have one power. I never <verb>. - Batman".
+
+def madlib(verb):
+	pass
+Example Usage
+
+verb = "give up"
+madlib(verb)
+
+verb = "nap"
+madlib(verb)
+Example Output:
+
+"I have one power. I never give up. - Batman"
+"I have one power. I never nap. - Batman"
+"""
+
+def madlib(verb):
+
+    print(f"I have one power. I never {verb} - Batman")
+
+verb = "give up"
+madlib(verb)
+
+verb = "nap"
+madlib(verb)
