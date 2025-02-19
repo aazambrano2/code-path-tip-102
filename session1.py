142dcdf8a96687ac271372a560557fa2453582ed
aazambrano2 <aazambrano2@miners.utep.edu>
2025-02-19 11:28:31 -0700

    p11

--- session1.py
+++ session1.py
@@ -285,3 +285,43 @@ print(split_haycorns(quantity))
 
 quantity = 1
 print(split_haycorns(quantity))
+
+"""
+Problem 11: T-I-Double Guh-ER
+Signs in the Hundred Acre Wood have been losing letters as Tigger bounces around stealing any letters he needs to spell out his name. 
+Write a function tiggerfy() that accepts a string s, and returns a new string with the letters t, i, g, e, and r from it.
+
+def tiggerfy(s):
+	pass
+Example Usage
+
+s = "suspicerous"
+tiggerfy(s)
+
+s = "Trigger"
+tiggerfy(s)
+
+s = "Hunny"
+tiggerfy(s)
+Example Output:
+
+"suspcous"
+""
+"Hunny"
+
+"""
+
+def tiggerfy(s):
+
+    s = [c for c in s if c.lower() not in "tiger"]
+
+    return "".join(s)
+
+s = "suspicerous"
+print(tiggerfy(s))
+
+s = "Trigger"
+print(tiggerfy(s))
+
+s = "Hunny"
+print(tiggerfy(s))
