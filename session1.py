861a142ffbd1ef842f8dde8bd02c572f313fc9ac
aazambrano2 <aazambrano2@miners.utep.edu>
2025-02-19 11:53:34 -0700

    pb 5 v2

--- session1.py
+++ session1.py
@@ -484,3 +484,37 @@ items = []
 print(get_last(items))
 
 
+"""
+Problem 5: Concatenate
+Write a function concatenate() that takes in a list of strings words and returns a string concatenated that concatenates all elements in words.
+
+def concatenate(words):
+	pass
+Example Usage
+
+words = ["vengeance", "darkness", "batman"]
+concatenate(words)
+
+words = []
+concatenate(words)
+Example Output:
+
+"vengeancedarknessbatman"
+""
+
+"""
+
+def concatenate(words):
+    if len(words) == 0: return ""
+    s = ""
+    for w in words:
+        s += w
+    #return "".join(words)
+    print(s)    
+    return s
+words = ["vengeance", "darkness", "batman"]
+concatenate(words)
+
+words = []
+concatenate(words)
+
