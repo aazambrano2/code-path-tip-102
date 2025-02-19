e865afc9a2c759fe7060f79063ce061071085463
aazambrano2 <aazambrano2@miners.utep.edu>
2025-02-19 11:10:54 -0700

    p3

--- session1.py
+++ session1.py
@@ -118,3 +118,39 @@ threshold = 4
 count_less_than(race_times, threshold)
 
 
+"""
+Problem 3: Catchphrase
+Write a function print_catchphrase() that accepts a string character as a parameter and prints the catchphrase of the given character as outlined in the table below.
+
+Character	Catchphrase
+"Pooh"	"Oh bother!"
+"Tigger"	"TTFN: Ta-ta for now!"
+"Eeyore"	"Thanks for noticing me."
+"Christopher Robin"	"Silly old bear."
+If the given character does not match one of the characters included above, print "Sorry! I don't know <character>'s catchphrase!"
+
+def print_catchphrase(character):
+	pass
+Example Usage
+
+character = "Pooh"
+print_catchphrase(character)
+
+character = "Piglet"
+print_catchphrase(character)
+Example Output:
+
+"Oh bother!"
+"Sorry! I don't know Piglet's catchphrase!"
+
+"""
+
+def print_catchphrase(character):
+   if character.lower() == "pooh": print("oh brother!")
+   elif character.lower() == "tigger": print(" TTFN: Ta-ta for now!")
+   elif character.lower() == "eeyore": print(" Thanks for noticing me")
+   elif character.lower() == "christopher robin": print("Silly old bear")
+   else: print(f"Sorry! I don't know {character}'s catchphrase!")
+    
+character = "Pooh"
+print_catchphrase(character)
