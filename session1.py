3b918e5b9da4e50f67f7b79ead144156a741f4dd
aazambrano2 <aazambrano2@miners.utep.edu>
2025-02-19 11:36:42 -0700

    p1 v2

--- session1.py
+++ session1.py
@@ -363,3 +363,23 @@ items = ["book", "bouncy ball", "leaf", "red balloon"]
 locate_thistles(items)
 
 #Standard Problem Set V2
+
+"""
+Problem 1: Batman
+Write a function batman() that prints the string "I am vengeance. I am the night. I am Batman!".
+
+def batman():
+	pass
+Example Usage:
+
+batman()
+Example Output:
+
+I am vengeance. I am the night. I am Batman!
+
+"""
+
+def batman():
+    print("I am vengeance. I am the night. I am Batman!")
+
+batman()
