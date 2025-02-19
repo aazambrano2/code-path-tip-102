9d435430d51b3e9d6f0b911e34d12f2a36f5a0bf
aazambrano2 <aazambrano2@miners.utep.edu>
2025-02-19 11:23:09 -0700

    pb 10

--- session1.py
+++ session1.py
@@ -252,3 +252,36 @@ print(can_pair(item_quantities))
 
 item_quantities = []
 print(can_pair(item_quantities))
+
+"""
+Problem 10: Split Haycorns
+Piglet's has collected a big pile of his favorite food, haycorns, and wants to split them evenly amongst his friends. 
+Write a function split_haycorns() to help Piglet determine the number of ways he can split his haycorns into even groups. 
+split_haycorns() accepts a positive integer quantity as a parameter and returns a list of all divisors of quantity.
+
+def split_haycorns(quantity):
+	pass
+Example Usage
+
+quantity = 6
+split_haycorns(quantity)
+
+quantity = 1
+split_haycorns(quantity)
+Example Output:
+
+[1, 2, 3, 6]
+[1]
+"""
+
+def split_haycorns(quantity):
+
+    divisors = [i for i in range(1, quantity + 1) if quantity % i == 0]
+
+    return divisors
+
+quantity = 6
+print(split_haycorns(quantity))
+
+quantity = 1
+print(split_haycorns(quantity))
