968564194e64a12f7c6a228d1d0ad34dba4f558b
aazambrano2 <aazambrano2@miners.utep.edu>
2025-02-19 12:11:55 -0700

    pb9 v2

--- session1.py
+++ session1.py
@@ -604,3 +604,35 @@ def find_villain(crowd, villain):
 crowd = ['Batman', 'The Joker', 'Alfred Pennyworth', 'Robin', 'The Joker', 'Catwoman', 'The Joker']
 villain = 'The Joker'
 print(find_villain(crowd, villain))
+
+"""
+Problem 9: Odd
+Write a function get_odds() that takes in a list of integers nums and returns a new list containing all the odd numbers in nums.
+
+def get_odds(nums):
+	pass
+Example Usage
+
+nums = [1, 2, 3, 4]
+get_odds(nums)
+
+nums = [2, 4, 6, 8]
+get_odds(nums)
+Example Output:
+
+[1, 3]
+[]
+"""
+
+def get_odds(nums):
+
+    odds = [ n for n in nums if n % 2 != 0]
+    print(odds)
+    return odds
+
+nums = [1, 2, 3, 4]
+get_odds(nums)
+
+nums = [2, 4, 6, 8]
+get_odds(nums)
+
