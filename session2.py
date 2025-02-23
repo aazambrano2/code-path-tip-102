58919670f04ae73d23492784d0144ef4e5a3e5c0
aazambrano2 <aazambrano2@miners.utep.edu>
2025-02-23 14:31:45 -0700

    pb3 adv v2

--- session2.py
+++ session2.py
@@ -221,3 +221,34 @@ print(is_palindrome(s))
 
 s = "madamweb"
 print(is_palindrome(s))
+
+"""
+Problem 3: Squash Spaces
+Write a function squash_spaces() that takes in a string s as a parameter and returns a new string with each substring with consecutive spaces reduced to a single space. Assume s can contain leading or trailing spaces, but in the result should be trimmed. Do not use any of the built-in trim methods.
+
+def squash_spaces(s):
+    pass
+Example Usage
+
+s = "   Up,     up,   and  away! "
+squash_spaces(s)
+
+s = "With great power comes great responsibility."
+squash_spaces(s)
+Example Output:
+
+"Up, up, and away!"
+"With great power comes great responsibility."
+
+"""
+
+def squash_spaces(s):
+    return " ".join(s.split())
+
+
+
+s = "   Up,     up,   and  away! "
+print(squash_spaces(s))
+
+s = "With great power comes great responsibility."
+print(squash_spaces(s))
