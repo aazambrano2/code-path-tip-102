ee3ec5100069882af62ecd62548cc2cac263088e
aazambrano2 <aazambrano2@miners.utep.edu>
2025-02-23 14:20:14 -0700

    pb1 adv v2

--- session2.py
+++ session2.py
@@ -113,3 +113,62 @@ most_honey(height)
 
 height = [1, 1]
 most_honey(height)
+
+
+#advance problem set v2
+
+"""
+Problem 1: Matrix Addition
+Write a function add_matrices() that accepts to n x m matrices matrix1 and matrix2. 
+The function should return an n x m matrix sum_matrix that is the sum of the given matrices such that each value in sum_matrix 
+is the sum of values of corresponding elements in matrix1 and matrix2.
+
+def add_matrices(matrix1, matrix2):
+    pass
+Example Usage
+
+matrix1 = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+matrix2 = [
+    [9, 8, 7],
+    [6, 5, 4],
+    [3, 2, 1]
+]
+
+add_matrices(matrix1, matrix2)
+Example Output:
+
+[
+    [10, 10, 10],
+    [10, 10, 10],
+    [10, 10, 10]
+]
+
+"""
+
+def add_matrices(matrix1,matrix2):
+
+    sum_matrix =[[0 for _ in range(len(matrix1))] for _ in range(len(matrix[0]))]
+    
+    for i in range(len(matrix1)):
+        for j in range(len(matrix1[0])):
+            sum_matrix[i][j] = matrix1[i][j] + matrix2[i][j]
+    return sum_matrix
+
+matrix1 = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+matrix2 = [
+    [9, 8, 7],
+    [6, 5, 4],
+    [3, 2, 1]
+]
+
+print(add_matrices(matrix1, matrix2))
