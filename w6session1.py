cf19184eb16335131940d3ff2b8f6226864275a9
aazambrano2 <aazambrano2@miners.utep.edu>
2025-03-25 18:50:35 -0600

    session 1 week 6 again

--- w6session1.py
+++ w6session1.py
@@ -1,8 +1,65 @@
+# class SongNode:
+#     def __init__(self, song, artist, next=None):
+#         self.song = song
+#         self.artist = artist
+#         self.next = next
+
+# # For testing
+# def print_linked_list(node):
+#     current = node
+#     while current:
+#         print(current.song, end=" -> " if current.next else "")
+#         current = current.next
+#     print()
+
+# # Test linked list
+# top_hits_2010s = SongNode("Uptown Funk", "Mark Ronson ft. Bruno Mars", 
+#                           SongNode("Party Rock Anthem", "LMFAO", 
+#                                    SongNode("Bad Romance", "Lady Gaga")))
+
+# song1 = SongNode("Uptown Funk", "Mark Ronson ft. Bruno Mars")
+# song2 = SongNode("Party Rock Anthem", "LMFAO")
+# song3 = SongNode("Bad Romance", "Lady Gaga")
+
+# song1.next = song2
+# song2.next = song3
+# print_linked_list(song1)
+
+# from collections import defaultdict
+
+# def get_artist_frequency(playlist):
+#     if not playlist:
+#         return {}
+
+#     frequency = defaultdict(int)  # Default value for frequencies is 0
+#     current = playlist
+#     while current:
+#         frequency[current.artist] += 1  # Increment artist frequency count
+#         current = current.next
+
+#     return frequency
+
+# # Create a playlist linked list
+# playlist = SongNode("Saturn", "SZA", 
+#                     SongNode("Who", "Jimin", 
+#                              SongNode("Espresso", "Sabrina Carpenter", 
+#                                       SongNode("Snooze", "SZA"))))
+
+# # Get artist frequencies
+# artist_freq = get_artist_frequency(playlist)
+# print(artist_freq)
+
+
+
+
+
+
 class SongNode:
     def __init__(self, song, artist, next=None):
         self.song = song
         self.artist = artist
         self.next = next
+
 
 # For testing
 def print_linked_list(node):
@@ -12,39 +69,64 @@ def print_linked_list(node):
         current = current.next
     print()
 
-# Test linked list
-top_hits_2010s = SongNode("Uptown Funk", "Mark Ronson ft. Bruno Mars", 
-                          SongNode("Party Rock Anthem", "LMFAO", 
-                                   SongNode("Bad Romance", "Lady Gaga")))
+# top_hits_2010s = SongNode("Uptown Funk", SongNode("Party Rock Anthem", SongNode("Bad Romance")))
+# print_linked_list(top_hits_2010s)
 
-song1 = SongNode("Uptown Funk", "Mark Ronson ft. Bruno Mars")
-song2 = SongNode("Party Rock Anthem", "LMFAO")
-song3 = SongNode("Bad Romance", "Lady Gaga")
+# node1 = SongNode("Uptown Funk")
+# node2 = SongNode("Party Rock Anthem")
+# node3 = SongNode("Bad Romance")
 
-song1.next = song2
-song2.next = song3
-print_linked_list(song1)
+# node1.next = node2
+# node2.next = node3
+
+# print_linked_list(node1)
 
 from collections import defaultdict
 
-def get_artist_frequency(playlist):
-    if not playlist:
-        return {}
+def get_artists_frequency(playlist):
+    """
 
-    frequency = defaultdict(int)  # Default value for frequencies is 0
+    u: return a dict with artists frequencies
+    m: dictionary and iteration
+    p:
+        we use a dictionary
+        while current is not none
+        add key value to 1 where are key is the artists name
+
+        return dictionary
+    """
+
+    frequency = defaultdict(int)
+
     current = playlist
+
+    if not playlist: return {}
+
     while current:
-        frequency[current.artist] += 1  # Increment artist frequency count
+        frequency[current.artist] += 1
         current = current.next
-
+    
     return frequency
+    
+# def get_artist_frequency(playlist):
+#     if not playlist:
+#         return {}
 
-# Create a playlist linked list
+#     frequency = defaultdict(int)  # Default value for frequencies is 0
+#     current = playlist
+#     while current:
+#         frequency[current.artist] += 1  # Increment artist frequency count
+#         current = current.next
+
+#     return frequency
+
+
 playlist = SongNode("Saturn", "SZA", 
-                    SongNode("Who", "Jimin", 
-                             SongNode("Espresso", "Sabrina Carpenter", 
-                                      SongNode("Snooze", "SZA"))))
+                SongNode("Who", "Jimin", 
+                        SongNode("Espresso", "Sabrina Carpenter", 
+                                SongNode("Snooze", "SZA"))))
 
-# Get artist frequencies
-artist_freq = get_artist_frequency(playlist)
-print(artist_freq)
+get_artists_frequency(playlist)
+
+print(get_artists_frequency(playlist))
+
